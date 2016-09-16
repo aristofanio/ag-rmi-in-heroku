7cc1ae6eee6a2b41e58af5fd0f745f4cfbbe4a30
Ari Garcia <aristofanio@hotmail.com>
2016-09-16 19:05:12 -0300

    deploy hello world

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* ag.ifpb.pod.rmi.heroku.Main
--- src/main/java/ag/ifpb/pod/rmi/heroku/Main.java
+++ src/main/java/ag/ifpb/pod/rmi/heroku/Main.java
@@ -4,7 +4,7 @@ package ag.ifpb.pod.rmi.heroku;
  * Hello world!
  *
  */
-public class App 
+public class Main 
 {
     public static void main( String[] args )
     {
