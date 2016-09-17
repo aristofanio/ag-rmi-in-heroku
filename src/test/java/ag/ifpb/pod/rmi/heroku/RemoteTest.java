dbb041406916dd348170114e28bc102f13ab7632
Ari Garcia <aristofanio@hotmail.com>
2016-09-17 00:01:49 -0300

    add test with 100 and 1000 threads (100 ok, 1000 fail)

--- src/test/java/ag/ifpb/pod/rmi/heroku/RemoteTest.java
+++ src/test/java/ag/ifpb/pod/rmi/heroku/RemoteTest.java
@@ -12,7 +12,7 @@ import junit.framework.TestCase;
 @SuppressWarnings("restriction")
 public class RemoteTest extends TestCase {
 
-  public void testApp() {
+  public void testApp() throws InterruptedException {
     try {
       //
       RMISocketFactory.setSocketFactory(new sun.rmi.transport.proxy.RMIHttpToCGISocketFactory());
@@ -22,10 +22,24 @@ public class RemoteTest extends TestCase {
           //"127.0.0.1", 1099, RMISocketFactory.getSocketFactory()
       );
       //
-      HelloWorldRemote remote = (HelloWorldRemote) registry.lookup(HelloWorldRemote.SERVICE_NAME);
-      //
+      final HelloWorldRemote remote = (HelloWorldRemote) registry.lookup(HelloWorldRemote.SERVICE_NAME);
       System.out.println(remote.helloWorld());
+      //força bruta... ;)
+      for (int i = 0; i < 100; i++) {//com 100 ok, mas com atraso. com 1000 quebra a execução
+        Thread thread = new Thread("pod-thread-" + i){
+          @Override
+          public void run() {
+            try {
+              System.out.println(Thread.currentThread().getName() + ": " + remote.helloWorld());
+            } catch (RemoteException e) {
+              e.printStackTrace();
+            }
+          }
+        };
+        thread.start();
+      }
       //
+      Thread.sleep(5000);
     } catch (RemoteException e) {
       e.printStackTrace();
     } catch (NotBoundException e) {
